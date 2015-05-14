dfe8f222923be96edc902710b547b99623d193c4
Jamie Stackhouse <jamie.stackhouse@redspace.com>
2015-05-14 17:27:28 -0300

    Cloudfront signatures.

--- cloudfront.go
+++ cloudfront.go
@@ -1,66 +1,122 @@
 package cloudfront
 
 import (
-        "crypto/rsa"
-        "encoding/base64"
-        "encoding/json"
-        "strings"
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"strings"
+	"time"
 )
 
 type CloudFront struct {
-        baseURL   string
-        keyPairId string
-        key       *rsa.PrivateKey
+	domain    string
+	KeyPairId string
+	key       *rsa.PrivateKey
+}
+
+type Policy struct {
+	policy []byte
+	cf     *CloudFront
 }
 
 type policy struct {
-        Statement []statement
+	Statement []statement
 }
 
 type statement struct {
-        Resource  string
-        Condition conditions
+	Resource  string
+	Condition conditions
 }
 
 type conditions struct {
-        DateLessThan    epochTime
-        DateGreaterThan *epochTime `json:",omitempty"`
-        IpAddress       *ipAddress  `json:",omitempty"`
+	DateLessThan    epochTime
+	DateGreaterThan *epochTime `json:",omitempty"`
+	IpAddress       *ipAddress `json:",omitempty"`
 }
 
 type epochTime struct {
-        Timestamp int64 `json:"AWS:EpochTime"`
+	Timestamp int64 `json:"AWS:EpochTime"`
 }
 
 type ipAddress struct {
-        Addr string `json:"AWS:SourceIp"`
+	Addr string `json:"AWS:SourceIp"`
 }
 
 // Will convert values from base64 encoded string to be URL safe
 var invalidReplacer = strings.NewReplacer("+", "-", "=", "_", "/", "~")
 
-func New(baseurl string, key *rsa.PrivateKey, keyPairId string) *CloudFront {
-        return &CloudFront{
-                baseURL:   baseurl,
-                keyPairId: keyPairId,
-                key:       key,
-        }
+var ErrMissingRequiredParam = errors.New("Missing required parameter")
+
+func New(key *rsa.PrivateKey, keyPairId string) *CloudFront {
+	return &CloudFront{
+		KeyPairId: keyPairId,
+		key:       key,
+	}
+}
+
+func (p Policy) Encode() string {
+	return invalidReplacer.Replace(base64.StdEncoding.EncodeToString(p.policy))
+}
+
+func (p Policy) Sign() (string, error) {
+	hash := hashSha(p.policy)
+	signed, err := rsa.SignPKCS1v15(nil, p.cf.key, crypto.SHA1, hash)
+	if err != nil {
+		return "", err
+	}
+
+	return invalidReplacer.Replace(base64.StdEncoding.EncodeToString(signed)), nil
+}
+
+func (cf *CloudFront) CreatePolicy(resource string, expiry time.Time, validAt *time.Time, ip *string) (Policy, error) {
+	if expiry.IsZero() {
+		return nil, ErrMissingRequiredParam
+	}
+
+	conds := conditions{
+		DateLessThan: epochTime{
+			Timestamp: expiry.Truncate(time.Millisecond).Unix(),
+		},
+	}
+
+	if validAt != nil {
+		conds.DateGreaterThan = &epochTime{
+			Timestamp: *validAt.Truncate(time.Millisecond).Unix(),
+		}
+	}
+
+	if ip != nil {
+		conds.IpAddress = &ipAddress{
+			Addr: *ip,
+		}
+	}
+
+	policy, err := buildPolicy(resource, conds)
+	if err != nil {
+		return nil, err
+	}
+
+	return Policy{cf: cf, policy: policy}, nil
+}
+
+func hashSha(policy []byte) []byte {
+	hash := sha1.New()
+	hash.Write(p.policy)
+	hash.Sum(nil)
 }
 
 func buildPolicy(resource string, conditions conditions) ([]byte, error) {
-    p := &policy{
-        Statement: []statement{
-            statement{
-                Resource: resource,
-                Condition: conditions,
-            },
-        },
-    }
-    
-    return json.Marshal(p)
-}
+	p := &policy{
+		Statement: []statement{
+			statement{
+				Resource:  resource,
+				Condition: conditions,
+			},
+		},
+	}
 
-// Helper to make a slice-o-bytes query string safe
-func queryize(policy []byte) string {
-    return invalidReplacer.Replace(base64.StdEncoding.EncodeToString(policy))
+	return json.Marshal(p)
 }
